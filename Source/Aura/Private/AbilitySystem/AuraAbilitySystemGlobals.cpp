57b148fb5134d19385604c36eac4995b7d13aa6e
Mezz <mezzanine510@gmail.com>
2025-01-06 23:27:54 -0800

    Engine now uses custom AuraGameplayEffectContext class by default

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Copyright Monkeyman Studios
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}

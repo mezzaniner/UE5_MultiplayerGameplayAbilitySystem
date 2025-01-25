c458accd4b943c649eece8141e535b344a3042f0
Mezz <mezzanine510@gmail.com>
2025-01-24 23:01:35 -0800

    added dissolve material to Shaman

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -50,9 +50,8 @@ void AAuraProjectile::BeginPlay()
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// TODO: Refactor for PvP with a more robust system that handles team detection
 	// Prevent friendly fire
-	// TODO: Refactor for PvP with more robust system
-	// if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	if (!DamageEffectSpecHandle.Data.IsValid()) return;
 	if (OtherActor == GetOwner()) return;
 	if (!UAuraAbilitySystemLibrary::IsNotFriend(DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser(),OtherActor)) return;

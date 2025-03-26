0fed4184615eafadd15b0613489cf5c2f6851400
Mezz <mezzanine510@gmail.com>
2025-03-25 17:08:01 -0700

    added a new level; pp and lighting adjustments

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -24,8 +24,7 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 	UFUNCTION(BlueprintCallable, Category = "Projectile")
-	void SpawnProjectile(const FVector& TargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch = false, 
-	float PitchOverride = 0.f);
+	void SpawnProjectile(const FVector& TargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch = false, float PitchOverride = 0.f);
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;

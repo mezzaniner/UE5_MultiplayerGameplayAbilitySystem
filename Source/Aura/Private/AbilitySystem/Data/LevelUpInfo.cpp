28b9bc87186814b12deb554f591c50460b98ea4e
Mezz <mezzanine510@gmail.com>
2025-03-28 13:34:19 -0700

    created LevelUpInfo data asset to level calculate XP requirements and rewards

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,27 @@
+// Copyright Monkeyman Studios
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(const int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// LeveUpInformation at 1 = Level 1 information
+		// LeveUpInformation at 2 = Level 2 information...
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequiredXP)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+
+	return Level;
+}
